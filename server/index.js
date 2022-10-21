36f9b900e62fd6cec3f0c33713b68f8011ff0d24
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-10-21 18:14:00 +0300

    added changes to server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,14 +9,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 app.post('/store-data', (req, res) => {
   const data = {
     name: req.body.name,
     email: req.body.email,
-    message: req.body.message
-  }
-  const sql = "INSERT INTO users SET ?";
-    
+    message: req.body.message,
+  };
+  const sql = 'INSERT INTO users SET ?';
+
   connection.query(sql, data, (err) => {
     if (err) {
       res.status(400).send('Error');
